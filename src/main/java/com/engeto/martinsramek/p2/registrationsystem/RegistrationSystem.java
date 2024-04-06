83ac796f12ac0c4b33678822c336fba85e16ea1b
Martin Šrámek <x.sramek@gmail.com>
2024-04-06 10:24:33 +0200

    Initial commit - creation of empty Spring Boot project

---
+++ src/main/java/com/engeto/martinsramek/p2/registrationsystem/RegistrationSystem.java
@@ -0,0 +1,13 @@
+package com.engeto.martinsramek.p2.registrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationSystem {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationSystem.class, args);
+	}
+
+}
---
+++ src/test/java/com/engeto/martinsramek/p2/registrationsystem/RegistrationSystemTests.java
@@ -0,0 +1,13 @@
+package com.engeto.martinsramek.p2.registrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationSystemTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
