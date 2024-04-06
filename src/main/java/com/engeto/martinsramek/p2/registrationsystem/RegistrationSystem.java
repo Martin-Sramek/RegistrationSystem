d342e143191eabfb751c48db6eced65fbb7e972b
Martin Šrámek <x.sramek@gmail.com>
2024-04-06 10:55:07 +0200

    DataSource created. Successful connection to MySQL default database.

--- src/main/java/com/engeto/martinsramek/p2/registrationsystem/RegistrationSystem.java
+++ src/main/java/com/engeto/martinsramek/p2/registrationsystem/RegistrationSystem.java
@@ -1,13 +1,31 @@
 package com.engeto.martinsramek.p2.registrationsystem;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class RegistrationSystem {
+@Log
+public class RegistrationSystem implements CommandLineRunner {
+
+	private DataSource dataSource;
+
+	public RegistrationSystem(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationSystem.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Datasource: " + dataSource.toString());
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.execute("select 1");
+	}
 }
