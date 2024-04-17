f81bb1bf960d62dba7a374d9baff627bdea72585
Martin Šrámek <x.sramek@gmail.com>
2024-04-17 20:12:35 +0200

    Discord user name was added as a comment in RegistrationSystem.java.

--- src/main/java/com/engeto/martinsramek/p2/registrationsystem/RegistrationSystem.java
+++ src/main/java/com/engeto/martinsramek/p2/registrationsystem/RegistrationSystem.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RegistrationSystem {
 
+	// uzivatelske jmeno na Discordu: martin_sramek
+
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationSystem.class, args);
 	}
